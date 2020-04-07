65822b69ce8d4395a3a6ae060b35055438392157
Peswani Yukta <yuktapeswani12@gmail.com>
2020-04-07 12:37:45 +0530

    middleware changes

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -11,7 +11,7 @@ class QuestionsController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth'])->except(['create','store','edit','update']);
+        $this->middleware(['auth'])->except(['index','show']);
 
     }
 
