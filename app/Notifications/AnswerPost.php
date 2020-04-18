27a4d316ab5df4dec8c09846d52d75576c3b4309
Peswani Yukta <yuktapeswani12@gmail.com>
2020-04-18 13:06:05 +0530

    notification on mail template added

---
+++ app/Notifications/AnswerPost.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class AnswerPost extends Notification
+{
+    use Queueable;
+    private $answerLink;
+    private $question;
+    private $answerBy;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @param $question
+     * @param $answerBy
+     * @param $answerLink
+     */
+    public function __construct($question, $answerBy,$answerLink)
+    {
+        $this->question = $question;
+        $this->answerBy = $answerBy;
+        $this->answerLink = $answerLink;
+
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('Your Question')
+                    ->line('' . $this->question . 'has a answer by ' . $this->answerBy)
+                    ->action('View Answer', url('/'.$this->answerLink));
+
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
